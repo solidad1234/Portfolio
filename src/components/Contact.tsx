891be67a01544e34e89746bcaf45ba435b29bd2b
solidad1234 <solidadkimeu@gmail.com>
2025-09-05 11:34:25 +0300

    removing react

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Mail, Phone, MapPin, Send, Linkedin, Github } from 'lucide-react';
 
 const Contact: React.FC = () => {
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { ArrowRight, Download, Github, Linkedin, Mail } from 'lucide-react';
 
 const Hero: React.FC = () => {
